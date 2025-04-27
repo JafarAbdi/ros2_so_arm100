53888d3377f020996ea43d21b639db9a396f534a
Yadunund <yadunund@gmail.com>
2025-04-26 23:52:26 -0700

    Namespace controllers
    Signed-off-by: Yadunund <yadunund@gmail.com>

--- so_arm100_description/launch/controllers_bringup.launch.py
+++ so_arm100_description/launch/controllers_bringup.launch.py
@@ -1,7 +1,11 @@
+import os
 from ament_index_python.packages import get_package_share_path
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from launch_ros.descriptions import ParameterFile
+from nav2_common.launch import ReplaceString, RewrittenYaml
 
 from so_arm100_description.launch_utils import launch_configurations, load_xacro
 
@@ -27,6 +31,7 @@ def make_robot_state_publisher_node(args):
         Node(
             package="robot_state_publisher",
             executable="robot_state_publisher",
+            namespace=str(args.namespace),
             parameters=[
                 {"robot_description": robot_description},
             ],
@@ -35,20 +40,46 @@ def make_robot_state_publisher_node(args):
 
 
 def generate_launch_description():
-    ros2_controllers_file = (
-        get_package_share_path("so_arm100_description")
-        / "control"
-        / "ros2_controllers.yaml"
+    bringup_dir = get_package_share_path("so_arm100_description")
+    ros2_controllers_file = os.path.join(
+        bringup_dir, "control", "ros2_controllers.yaml"
+    )
+
+    namespaced_ros2_controllers_file = ParameterFile(
+        RewrittenYaml(
+            source_file=ros2_controllers_file,
+            root_key=LaunchConfiguration('namespace'),
+            param_rewrites={},
+            convert_types=True,
+        ),
+        allow_substs=True,
+    )
+
+    # Define namespace as a launch argument
+    namespace_arg = DeclareLaunchArgument(
+        'namespace',
+        default_value='',
+        description='Namespace for the controller_manager and related nodes'
+    )
+
+    # Define controllers launch arguments
+    controllers_arg = DeclareLaunchArgument(
+        'controllers',
+        default_value=','.join(startup_controllers),
+        description='Comma-separated list of controllers to start'
     )
 
     return LaunchDescription(
         [
             DeclareLaunchArgument("hardware_type", default_value="mock_components"),
             DeclareLaunchArgument("usb_port", default_value="/dev/LeRobotFollower"),
+            namespace_arg,
+            controllers_arg,
             Node(
                 package="controller_manager",
                 executable="ros2_control_node",
-                parameters=[ros2_controllers_file],
+                namespace=LaunchConfiguration('namespace'),
+                parameters=[namespaced_ros2_controllers_file],
                 remappings=[("~/robot_description", "/robot_description")],
                 # To get logs from spdlog
                 output="screen",
@@ -61,6 +92,7 @@ def generate_launch_description():
             Node(
                 package="controller_manager",
                 executable="spawner",
+                namespace=LaunchConfiguration('namespace'),
                 arguments=[controller],
             )
             for controller in startup_controllers
