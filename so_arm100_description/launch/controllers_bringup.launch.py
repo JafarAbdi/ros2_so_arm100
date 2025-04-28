ffcb300694fd841c84ad4d8f291b900d9ba0202d
Yadunund <yadunund@gmail.com>
2025-04-27 22:51:54 -0700

    Allow robot links and joints to be namespaced
    Signed-off-by: Yadunund <yadunund@gmail.com>

--- so_arm100_description/launch/controllers_bringup.launch.py
+++ so_arm100_description/launch/controllers_bringup.launch.py
@@ -2,6 +2,7 @@ import os
 from ament_index_python.packages import get_package_share_path
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from launch_ros.descriptions import ParameterFile
@@ -18,11 +19,15 @@ startup_controllers = [
 
 @launch_configurations
 def make_robot_state_publisher_node(args):
+    namespace :str = args.namespace
+    if len(namespace) > 0:
+        namespace = namespace + "/"
     robot_description = load_xacro(
         get_package_share_path("so_arm100_description")
         / "urdf"
         / "so_arm100.urdf.xacro",
         mappings={
+            "prefix": namespace,
             "ros2_control_hardware_type": args.hardware_type,
             "usb_port": args.usb_port
         },
@@ -31,7 +36,7 @@ def make_robot_state_publisher_node(args):
         Node(
             package="robot_state_publisher",
             executable="robot_state_publisher",
-            namespace=str(args.namespace),
+            namespace=namespace,
             parameters=[
                 {"robot_description": robot_description},
             ],
@@ -41,18 +46,43 @@ def make_robot_state_publisher_node(args):
 
 def generate_launch_description():
     bringup_dir = get_package_share_path("so_arm100_description")
+
+
     ros2_controllers_file = os.path.join(
         bringup_dir, "control", "ros2_controllers.yaml"
+    )
+
+    namespace = LaunchConfiguration('namespace')
+    use_namespace = LaunchConfiguration('use_namespace')
+    # Only it applys when `use_namespace` is True.
+    # '<robot_namespace>' keyword shall be replaced by 'namespace' launch argument
+    # in config file 'ros2_controllers.yaml' as a default & example.
+    # User defined config file should contain '<robot_namespace>' keyword for the replacements.
+    ros2_controllers_file = ReplaceString(
+        source_file=ros2_controllers_file,
+        replacements={'<robot_namespace>': (namespace, '/')},
+        condition=IfCondition(use_namespace),
+    )
+    ros2_controllers_file = ReplaceString(
+        source_file=ros2_controllers_file,
+        replacements={'<robot_namespace>': ('')},
+        condition=UnlessCondition(use_namespace),
     )
 
     namespaced_ros2_controllers_file = ParameterFile(
         RewrittenYaml(
             source_file=ros2_controllers_file,
-            root_key=LaunchConfiguration('namespace'),
+            root_key=namespace,
             param_rewrites={},
             convert_types=True,
         ),
         allow_substs=True,
+    )
+
+    use_namespace_arg = DeclareLaunchArgument(
+        'use_namespace',
+        default_value='false',
+        description='Whether to apply a namespace to the controller stack',
     )
 
     # Define namespace as a launch argument
@@ -62,19 +92,12 @@ def generate_launch_description():
         description='Namespace for the controller_manager and related nodes'
     )
 
-    # Define controllers launch arguments
-    controllers_arg = DeclareLaunchArgument(
-        'controllers',
-        default_value=','.join(startup_controllers),
-        description='Comma-separated list of controllers to start'
-    )
-
     return LaunchDescription(
         [
             DeclareLaunchArgument("hardware_type", default_value="mock_components"),
             DeclareLaunchArgument("usb_port", default_value="/dev/LeRobotFollower"),
+            use_namespace_arg,
             namespace_arg,
-            controllers_arg,
             Node(
                 package="controller_manager",
                 executable="ros2_control_node",
